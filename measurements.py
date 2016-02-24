6a13dd251009515d976dd8d241575244a9aa3c55
sbaek_lab <sbaek@aelrayyah-ws.enphaseenergy.com>
2016-02-24 14:14:21 -0800

    initial update

---
+++ measurements.py
@@ -0,0 +1,75 @@
+"""
+Created on 02/24/2016, @author: sbaek
+  V00
+  - initial release
+"""
+
+import pandas as pd
+
+import sys, time
+from os.path import abspath, dirname
+sys.path.append(dirname(dirname(__file__)))        
+sys.path.append('%s/equipment' % (dirname(dirname(__file__)))) 
+import equipment.sas as sas
+import equipment.power_meter as pm
+from data_aq_lib.measurement import serial_commands as sc
+
+class measurement:
+    """
+           __INIT__     
+           __STR__      
+           
+    inputs
+            para=                - type:dict
+            equip=                - type:dict  
+            
+    """    
+    
+    def __init__(self, para, equip):              
+        self.return_str= ''   
+        self.par=para
+        self.eq=equip
+        self.results=pd.DataFrame()
+        self.flag='up'        
+        
+        try:self.pcu=equip['pcu']
+        except:pass
+    
+    def __str__(self):
+        self.return_str= '\n\n -- class measurement --' 
+        self.return_str += '\n data_path : %s  ' %self.par['data_path']
+        self.return_str += '\n source_path : %s  ' %self.par['source_path']
+        return self.return_str       
+
+
+    def do_measure(self):     
+        ''' initiation '''
+        time.sleep(3)
+        sas.sas_fixed(self.eq, CURR=15, VOLT=self.par['SAS_volt'])
+        time.sleep(1)
+
+        data=[] 
+        if type(self.par['Load_pts']) is str:
+            print '\n load pts is not specified'
+            self.par['load']=''
+            item=pm.pm_measure(self.eq)
+            item.update({'scan_time':time.strftime('%H:%M:%S')})
+            data.append(item) 
+                
+        elif type(self.par['Load_pts']) is not str:
+            for i in self.par['Load_pts']:
+                sc.command_power_SerialCom(self.pcu, float(i), self.par, self.eq, adj='On')                                  
+                
+                item=pm.pm_measure(self.eq)
+                item.update({'load_set':int(100*float(i))})  # add load information on para in dict format 
+                
+                load= 100*float(item['p_ac_out'])/float(self.par['p_rated'])                
+                item.update({'load':load})  # add load information on para in dict format 
+                item.update({'scan_time':time.strftime('%H:%M:%S')})
+                data.append(item)       
+                time.sleep(4)
+                
+        self.results=data 
+
+
+
---
+++ table_generator.py
@@ -0,0 +1,79 @@
+"""
+Created on 02/04/2016, @author: sbaek
+  V00
+  - initial release
+"""
+
+
+import sys, time
+from os.path import abspath, dirname
+sys.path.append(dirname(dirname(__file__)))        
+sys.path.append('%s/data_aq_lib' % (dirname(dirname(__file__)))) 
+
+import pandas as pd
+#import data_aq_lib.measurement.measurements as mm
+import equipment.power_meter as pm
+
+class table_gen:
+    """
+           __INIT__     
+           __STR__      
+           
+    inputs
+            para=                - type:dict
+            equip=                - type:dict  
+            
+    """    
+    
+    def __init__(self, para, equip):              
+        self.return_str= ''   
+        self.par=para
+        self.eq=equip
+        self.pcu=equip['pcu']
+
+    def __str__(self):
+        self.return_str= '\n\n class table_gen \n' 
+        self.return_str += '\n data_path : %s  ' %self.par['data_path']
+        self.return_str += '\n source_path : %s  ' %self.par['source_path']
+        #return_str += '\n save : %s  ' %self.new_file
+        return self.return_str    
+    
+    def generate(self, SAS_volt=32, show ='On', max_power=280):    
+        # return flag 'down' in case of failure of boot-up
+        global data
+        power_limit= 300.0
+        d, h, pi,po=[], [], [], []
+        """ in case of out of tune, power can flow in opposite direction!! carefull"""
+          
+#        self.pcu.debugger_cmd('pt 2\r') 
+#        time.sleep(2)
+  
+        start, end =20, 280    
+        hexa=hex(start).split('x')[1]
+        self.pcu.debugger_cmd('p %s\r' %hexa)  
+        time.sleep(4)  # need time to settle down        
+        
+        for i in range(start, end, 1):    
+            time.sleep(0.4)  # power is not measured in real time when avg function is on at wt500
+            dec=i
+            hexa=hex(dec).split('x')[1]
+            print '\n command p %s in hex (%s in dec)' %(hexa, dec)   
+            self.pcu.debugger_cmd('p %s\r' %hexa)  
+            #self.pcu.debugger_cmd('p %02X\r' %dec)  
+            item=pm.pm_measure(self.eq)            
+            d.append({'dec':dec,'hexa':hexa,'p_in':item['p_in'],'p_ac_out':item['p_ac_out']})              
+            df=pd.DataFrame(d)    #df should not be declared as global in main script.. kept updated..          
+            if show =='On': print '\n pin : %.2f W, pout: %.2f W,  Vdc:%.2f V' %(item['p_in'], item['p_ac_out'], item['volt_in'])
+            
+            if len(d)>8:
+                if int(d[i-start]['p_ac_out']-d[i-start-6]['p_ac_out']) <4 or d[i-start]['p_ac_out']>power_limit:break  #if output power is not increase enough comparing to Po prior to 5 pts                                             
+            else:
+                pass
+            
+            if (int(item['p_ac_out'])>max_power): #limite the power at 280W in
+                break
+        
+        name=self.par['source_path']+'/%s.csv' %'command_table'
+        print '\n save file to %s' %name
+        df.to_csv(self.par['source_path']+'/%s.csv' %'command_table') 
+        print  '\n %s' %time.strftime("%a, %d %b %Y %H:%M:%S")
