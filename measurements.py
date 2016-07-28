fc355a960ace19e3b9c9ad67cb8006481d4dc5f9
sbaek_lab <sbaek@aelrayyah-ws.enphaseenergy.com>
2016-07-28 10:52:02 -0700

    major changes - add functions to modify registers

--- measurements.py
+++ measurements.py
@@ -2,7 +2,13 @@
 Created on 02/24/2016, @author: sbaek
   V00
   - initial release
- 
+
+  V01 07/06/2016
+  - def check_fault()
+
+  V02 07/27/2016
+  - bootup process has been taken out and moved to main script.  It needs to be there to change registers.
+
 """
 
 import pandas as pd
@@ -19,141 +25,176 @@ import data_aq_lib.equipment.ac_source as ac
 
 class Measurement:
     """
-           __INIT__     
-           __STR__      
-           
+           __INIT__
+           __STR__
+
     inputs
             para=                - type:dict
-            equip=                - type:dict  
-            
-    """    
-    
-    def __init__(self, para, equip):              
-        self.return_str= ''   
+            equip=                - type:dict
+
+    """
+
+    def __init__(self, para, equip):
+        self.return_str= ''
         self.par=para
         self.eq=equip
         self.results=pd.DataFrame()
-        self.flag='up'        
-        
+        self.flag='up'
+
         try:self.pcu=equip['pcu']
         except:pass
-    
+
     def __str__(self):
         self.return_str= '\n\n -- Measurement --'
         self.return_str += '\n data_path : %s  ' %self.par['data_path']
         self.return_str += '\n source_path : %s  ' %self.par['source_path']
-        return self.return_str       
+        return self.return_str
 
 
-    def do_measure_pm(self):     
-   
-        ''' boot-up '''
-        ac.set_ac_source(self.eq, mode=self.par['ac_mode'], freq=60.0)
-        sas.sas_pcu_boot(self.eq, CURR=14, VOLT=self.par['SAS_volt'] , ADJ='On')     
+    def do_measure_pm(self, delay=1, adj=True, check_fault=True, Plim=305, show=False, boot_up=1.0):
+        #ac.set_ac_source(self.eq, mode=self.par['ac_mode'], freq=60.0)
+        #sas.sas_pcu_boot(self.eq, self.par, CURR=14, VOLT=self.par['SAS_volt'], boot_up=boot_up)
 
-        data=[] 
+        data=[]
         if type(self.par['Load_pts']) is str:
             print '\n load pts is not specified'
             self.par['load']=''
             item=pm.pm_measure(self.eq)
             item.update({'scan_time':time.strftime('%H:%M:%S')})
-            data.append(item) 
-                
+            data.append(item)
+
         elif type(self.par['Load_pts']) is not str:
             for i in self.par['Load_pts']:
-                sc.command_p(float(i), self.par, self.eq, adj='On')
+                sc.command_p(float(i), self.par, self.eq, adj=adj, delay=1.5, show=False) #taking data from pm takes time.
                 
+                ''' fault check '''
+                # if power output is abnormally low, it is considered as a fault and shutdown
+                if check_fault:
+                    for j in range(1, 4):
+                        print '.'
+                        flag=self.check_fault()
+                        if flag: break
+                        else:pass
+                        time.sleep(j*0.25) # check 4 time
+                else:
+                    pass
+                    
+                #sc.command_p(float(i), self.par, self.eq, adj=adj, delay=0)
                 item=pm.pm_measure(self.eq)
-                item.update({'load_set':int(100*float(i))})  # add load information on para in dict format 
-                
-                load= 100*float(item['p_ac_out'])/float(self.par['p_rated'])                
-                item.update({'load':load})  # add load information on para in dict format
-                
-                timestr=time.strftime("%I:%M:%S")
-                datestr=time.strftime(" %m/%d/%Y")
-                item.update({'scan_time':timestr})
-                item.update({'scan_date':datestr})         
-                
-                data.append(item)       
-                time.sleep(2)                
-        self.results=data 
+                load= 100*float(item['p_ac_out'])/float(self.par['p_rated'])
+                item.update({'Vdc':self.par['SAS_volt'], 'load_set':int(100*float(i)),'load':load, 'fault':flag})
+
+                try:
+                    temp=dvm.measure_tempc(self.eq)
+                    item.update({'Temp':temp})
+                except: pass
+                data.append(item)
+
+                if show:
+                    try:
+                        msg = '\n measurement : \n'
+                        msg +='  Po : %.1fW, Io : %.2f Arms, Pin : %.1fW, Vdc : %.1fV,\n' %(float(item['p_ac_out']), float(item['amp_ac_out1']),float(item['p_in']),float(item['volt_in']))
+                        msg +='  Load : %.1f%% at %.0fW, Eff. : %.1f%% \n' %(100*float(item['p_ac_out'])/self.par['p_rated'], self.par['p_rated'], float(item['eff'])) # put this in try, except. sometimes eff is not available
+                        print msg
+                        self.par['log'] +=msg
+                    except:pass
+
+                if (flag) or (float(item['p_ac_out'])>Plim): # limit output at 305W (default)
+                    break
+                    
+        self.results=data
+        return data
+
+    def check_fault(self):
+        # go to the next voltage if power is smaller than the smallest power requested
+        item=pm.pm_measure(self.eq) # to detect fault quickly, get measurement first
+        #if float(item['p_ac_out'])< (min(self.par['Load_pts']))*self.par['p_rated']:# does not work, pcu often cannot carry as requested
+        #print float(item['p_ac_out']), (0.5*min(self.par['Load_pts']))*self.par['p_rated']
+        if float(item['p_ac_out'])< (0.9*min(self.par['Load_pts']))*self.par['p_rated']:
+            self.shutdown()
+            msg=' fault..'
+            print msg
+            self.par['log'] +=msg
+            flag=True
+        else: flag=False
+        return flag
 
 
-    def do_measure_tempc(self,time_step=1, duration=1, SAT='off',POWER_METER='On'):     
-        
-        data = []    #collection of item in dict()  !!!!  
+    def do_measure_tempc(self,time_step=1, duration=1, SAT='off',POWER_METER='On'):
+        data = []    #collection of item in dict()  !!!!
         cnt, cnt_max = 0, int(duration/time_step)
-        
+
         ''' temperature saturation check '''
         if SAT=='On':
             #data, temp_ini, t=self.check_tempc_sat(time_step=time_step, tolerance=0.3)
             data, temp_ini, t=self.check_tempc_sat(time_step=time_step, tolerance=0.1)
         else:pass
-    
+
         ''' boot-up '''
         ac.set_ac_source(self.eq, mode=self.par['ac_mode'], freq=60.0)
-        item=sas.sas_pcu_boot(self.eq, CURR=14, VOLT=self.par['SAS_volt'])      #receive the time of boot-up         
+        item=sas.sas_pcu_boot(self.eq, CURR=14, VOLT=self.par['SAS_volt'])      #receive the time of boot-up
         data.append(item)   #inital value when boot-up
-        
-                        
-        while cnt<cnt_max:                                
-            if (cnt==0 or (time.clock()-t)>time_step*60):    
+
+
+        while cnt<cnt_max:
+            if (cnt==0 or (time.clock()-t)>time_step*60):
                 t=time.clock()  # this update has to be done first, otherwise the processing time is also added.
-                cnt=cnt+1     
-                
-                sc.command_p(float(1.0), self.par, self.eq, adj='Off')          #measurement temperature first before any adjustment.                          
+                cnt=cnt+1
+
+                sc.command_p(float(1.0), self.par, self.eq, adj='Off')          #measurement temperature first before any adjustment.
                 temp=dvm.measure_tempc(self.eq)
                 item=({'Temp':temp, 'Temp_i':temp_ini, 'Temp_delta':temp-temp_ini})
                 timestr=time.strftime("%I:%M:%S")
                 datestr=time.strftime(" %m/%d/%Y")
                 item.update({'scan_time':timestr, 'scan_date':datestr})
-                
-                sas.sas_fixed_adj(self.eq, CURR=14, VOLT=self.par['SAS_volt'])  #adjust Vdc before pm measurement            
-                sc.command_p(float(1.0), self.par, self.eq, adj='On')           #adjust Po before pm measurement                            
-                if POWER_METER=='On':
-                    item.update(pm.pm_measure(self.eq))                         #item in dict           
-                else: pass           
-                data.append(item)                                          
-        self.results=data                                    #data is list with elements in dict
 
-   
-    def check_tempc_sat(self, time_step=1, tolerance=0.1):       
+                sas.sas_fixed_adj(self.eq, CURR=14, VOLT=self.par['SAS_volt'])  #adjust Vdc before pm measurement
+                sc.command_p(float(1.0), self.par, self.eq, adj='On')           #adjust Po before pm measurement
+                if POWER_METER=='On':
+                    item.update(pm.pm_measure(self.eq))                         #item in dict
+                else: pass
+                data.append(item)
+        self.results=data                                    #data is list with elements in dict
+        return data
+
+    def check_tempc_sat(self, time_step=1, tolerance=0.1):
         #checking temperature is saturated
-        data=[]         
+        data=[]
         t=time.clock()
-        cnt, cnt_max=0, 20  #max 20min        
+        cnt, cnt_max=0, 20  #max 20min
         a=[]    #temperary list for temperature
-        
-        while (cnt<cnt_max):                                
-            if (cnt==0 or (time.clock()-t)>time_step*60):   #convert time step to minute    
+
+        while (cnt<cnt_max):
+            if (cnt==0 or (time.clock()-t)>time_step*60):   #convert time step to minute
                 t=time.clock()                              # this update has to be done first, otherwise the processing time is also added.
-                cnt=cnt+1                     
-                item=pm.pm_measure(self.eq)                 #item in dict    
+                cnt=cnt+1
+                item=pm.pm_measure(self.eq)                 #item in dict
                 timestr=time.strftime("%I:%M:%S")
                 datestr=time.strftime(" %m/%d/%Y")
                 item.update({'scan_time':timestr})
                 item.update({'scan_date':datestr})
                 temp=dvm.measure_tempc(self.eq)
-                item.update({'Temp':temp})                
-                data.append(item)         
-                
+                item.update({'Temp':temp})
+                data.append(item)
+
                 a.append(temp)
                 cnt_rec=3
                 if cnt >cnt_rec:
-                    diff=max(a[-cnt_rec:])-min(a[-cnt_rec:])                    
+                    diff=max(a[-cnt_rec:])-min(a[-cnt_rec:])
                     if diff<tolerance:
                         temp_ini=sum(a[-cnt_rec:]) / float(len(a[-cnt_rec:]))
-                        break                               
+                        break
                 else:pass
         return data[-cnt_rec:], temp_ini, time.clock()
-        
-        
-    def shutdown(self):          
-        print '\n ================== shut down =================='
-        print ' %s' %time.strftime("%I:%M:%S")
+
+    def shutdown(self):
         sas.sas_off(self.eq)
         time.sleep(2)
-        ac.ac_off(self.eq)        
-        print ' ===============================================\n'
-        
-        
+        ac.ac_off(self.eq)
+        msg ='\n ================== shut down =================='
+        msg +=' %s\n' %time.strftime("%I:%M:%S")
+        print msg
+        self.par['log'] +=msg
+        time.sleep(5)
+
+
--- serial_commands.py
+++ serial_commands.py
@@ -8,7 +8,11 @@ Created on 02/24/2016, @author: sbaek
 
   V02 04/06/2016
   - no longer use table.csv.
+  - 1 bit =0.01/128.0 = 0.078mA, e.g. dc_step=50, 0.01/128.0*50 = 4mA -> 0.96W
   - add com_adj(load, para, equip, bit, step)
+
+  V03 07/06/2016
+  - write(cmd, delay)
 
 """
 import sys, time
@@ -21,61 +25,80 @@ from data_aq_lib.equipment import serialcom
 import data_aq_lib.equipment.power_meter as pm
 
 
-def command_p(load, para, equip, adj='On', dec_step=50, tolerence=1.5): #, tolerance in Watt,  asj is added in case simply need to put command
+def command_p(load, para, equip, adj=True, dec_step=50, tolerence=1.5, delay=1, show=True): #,
     '''
-    requires 'command_table.csv' in folder 'source_file'
-    the table has,at most, 187 values   
-    pts : list of load condition out of rated power in range 0 and 1.0
-        e.g. [0.5,0.75,1.0]
-        
-    1 bit =0.01/128.0 = 0.078mA
-    dc_step=50, 0.01/128.0*50 = 4mA -> 0.96W
-    '''
-    ser=serialcom.SerialCom()    
+    :param load: float portion of para['p_rated']  e.g. 0.5
+    :param para: dict() with para['p_rated']
+    :param equip: dict()
+    :param adj: Bool
+    :param dec_step: int
+    :param tolerence: float
+    :return: None
 
+    - tolerance in Watt,  asj is added in case simply need to put command
+    - it takes time to receive data from pm.  it can be skipped by setting show.
+    '''
+    ser=serialcom.SerialCom()
     p_rated= para['p_rated']
    
     if para['ac_mode'] =='LL':
-        Ipeak=p_rated*load/240.0*2**.5  #'LL' mode, 240Vrm
-        #print '\n amp_ac : %.2f Arms' %(Ipeak/2**0.5)        
-        bit=int(Ipeak/(0.01/128))   
-    
-        ser.write(cmd='pt 2\r')
-        time.sleep(2)
+        Po=p_rated*load
+        Ipeak=Po/240.0*2**.5  #'LL' mode, 240Vrm
+        #print '\n Po : %.1f, amp_ac : %.2f Arms at %.1fW, P_rated=%.1fW ' %(Po, Ipeak/2**0.5, Po, p_rated)        
+        bit=int(Ipeak/(0.01/128))
+        ser.write(cmd='pt 2\r', delay=delay)
         
-        diff, bit, eff=com_adj(load, para, equip, bit, step=0)  #command as calculated
-        time.sleep(1)
-    
-        if (adj=='On'):        
+        msg = '\n command :\n'
+        msg += '  Po : %.1f, Io : %.2f Arms, load: %.2f\n' %(Po, Ipeak/2**0.5, load)
+
+        print msg
+        para['log'] +=msg
+        
+        if adj:        
+            diff, bit, eff=com_adj(ser, load, para, equip, bit, step=0, delay=delay)  #command as calculated
             for i in range(1, 20):  
                 if ( abs(diff) < tolerence ):
                     break
                 elif ( diff < -tolerence*3.5 ):
-                    diff, bit, eff=com_adj(load, para, equip, bit, step=dec_step*3) 
+                    diff, bit, eff=com_adj(ser,load, para, equip, bit, step=dec_step*3, delay=delay)
                 elif ( diff < -tolerence*2.5 ):
-                    diff, bit, eff=com_adj(load, para, equip, bit, step=dec_step*2)                    
+                    diff, bit, eff=com_adj(ser,load, para, equip, bit, step=dec_step*2, delay=delay)
                 elif ( diff < -tolerence*1.5 ):
-                    diff, bit, eff=com_adj(load, para, equip, bit, step=dec_step*1)                    
+                    diff, bit, eff=com_adj(ser,load, para, equip, bit, step=dec_step*1, delay=delay)
                 elif ( diff > tolerence*1.5 ):
-                    diff, bit, eff=com_adj(load, para, equip, bit, step=-dec_step*1)   
+                    diff, bit, eff=com_adj(ser,load, para, equip, bit, step=-dec_step*1, delay=delay)
                 elif ( diff > tolerence*2.5 ):
-                    diff, bit, eff=com_adj(load, para, equip, bit, step=-dec_step*2)
-                    
-            item=pm.pm_measure(equip)    
-            print '\n Load : %.1f%% at P_rated=%sW, Eff.=%.1f%%' %(100*float(item['p_ac_out'])/para['p_rated'], para['p_rated'], float(item['eff']))
+                    diff, bit, eff=com_adj(ser,load, para, equip, bit, step=-dec_step*2, delay=delay)
+                else:pass
+        else:
+            p_hex=hex(bit)
+            p_hex=p_hex.split('x')[1]
+            ser.write(cmd='p %s\r' %p_hex, delay=delay)
 
+        if show:show(equip)      # taking data from pm takes time.
+        else:pass
+    ser.close()
 
-def com_adj(load, para, equip, bit, step=0):    
-    ser=serialcom.SerialCom()    
+def com_adj(ser,load, para, equip, bit, step=0, delay=1):
     p_rated= para['p_rated']
     bit=bit+step
-    
-    if bit < 22390:     #~290W max
-        p_hex=hex(bit+step)        
-        p_hex=p_hex.split('x')[1]
-        ser.write(cmd='p %s\r' %p_hex)
-        
-        time.sleep(1) #pcu reponse is slower than time.sleep(1.0)!!!!        
-        item=pm.pm_measure(equip)
-        diff=item['p_ac_out']-p_rated*load  #update diff
+
+    #if bit < 22390:     #~290W max    
+    p_hex=hex(bit+step)
+    p_hex=p_hex.split('x')[1]
+    ser.write(cmd='p %s\r' %p_hex, delay=delay)
+
+    time.sleep(1) #pcu reponse is slower than time.sleep(1.0)!!!!
+    item=pm.pm_measure(equip)
+    diff=item['p_ac_out']-p_rated*load  #update diff
     return diff, bit, float(item['eff'])
+
+def show(equip):
+    item=pm.pm_measure(equip)
+    try:        
+        print '\n measurement :'
+        print '  Po : %.1fW, Io : %.2fArms, Pin : %.1fW, Vdc : %.1fV,' %(float(item['p_ac_out']), float(item['amp_ac_out1']),float(item['p_in']),float(item['volt_in']))
+        print '  Load : %.1f%% at %sW, Eff. : %.1f%%' %(100*float(item['p_ac_out'])/para['p_rated'], para['p_rated'], float(item['eff']))
+    except:pass
+
+
